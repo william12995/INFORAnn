88886deff19ac9540ae807746fa18b5bddb55190
Leo Chen <leo881003@gmail.com>
2016-07-12 23:30:58 +0800

    uncomplete code...

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 ﻿var mongoose = require('mongoose');
-var Todo = mongoose.model('Todo');
+var querystring = require('querystring');
+var Ann = mongoose.model('Ann');
 /*
  * GET home page.
  */
 
 exports.index = function (req, res) {
-    res.render('index', { title: 'INFOR Ann System' });
+    var reqdata = querystring.parse(req.url.query);
+    Ann.find({ visible : true }).sort('-update').exec(function (err, data)
+        {
+            
+        }
+    )
+    //res.render('index', { title: 'INFOR Ann System' });
 };
