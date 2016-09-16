bc04c97db889032a262a418e57558b27f827bb2f
Leo Chen <leo881003@gmail.com>
2016-09-16 21:03:49 +0800

    Ace Editor seems to work great!

--- public/js/acesync.js
+++ public/js/acesync.js
@@ -1,13 +1,24 @@
 var textarea = document.getElementById('content');
+var mode = document.getElementById('istextcontent');
 
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/tomorrow_night");
-editor.getSession().setMode("ace/mode/html");
+modeswitch();
 editor.getSession().setTabSize(4);
 editor.getSession().setUseSoftTabs(true);
-document.getElementById('editor').style.fontSize='14px';
+editor.getSession().setUseWorker(false);
 
 editor.getSession().on('change', function () {
     textarea.value=editor.getSession().getValue();
 });
 textarea.value=editor.getSession().getValue();
+
+mode.onclick = modeswitch;
+
+function modeswitch() {
+	if(mode.checked){
+		editor.getSession().setMode("ace/mode/html");
+	} else {
+		editor.getSession().setMode("");
+	}
+}
