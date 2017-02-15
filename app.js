94e3a8a9bf05a43026ad497a2ff6a057753cd3f7
LeoChen <leo881003@gmail.com>
2017-02-15 13:22:23 +0800

    Rewrite Linebot as a module(not tested)

--- app.js
+++ app.js
@@ -17,14 +17,12 @@ var session = require('express-session');
 var debug = require('debug')('inforann:server');
 var colors = require('colors');
 var moment = require('moment');
-var request = require('request');
 var CronJob = require('cron').CronJob;
 
 var index = require('./routes');
 var admin = require('./routes/admin');
 var embed = require('./routes/embed');
 var Admin = mongoose.model('Admin');
-var Line = mongoose.model('Line');
 var Session = mongoose.model('Session');
 
 var app = express();
@@ -81,124 +79,12 @@ var dbclean = new CronJob({
 });
 dbclean.start();
 
-global.linebot = {};
-
-function disable() {
-    console.log('[WARN]'.yellow + 'unable to read linebot config');
-    console.log('[WARN]'.yellow + 'will disable linebot function');
-    linebot.cfg = {
-        enable: false,
-        token: "",
-        secret: ""
-    };
-}
-try {
-    var data = fs.readFileSync('linebot.json', {
-        encoding: 'utf-8',
-        flag: 'r'
-    });
-    linebot.cfg = JSON.parse(data);
-    if (linebot.cfg.token && linebot.cfg.secret) {
-        if(!linebot.cfg.enable)
-            linebot.cfg.enable = true;
-        if (!linebot.cfg.debug) linebot.cfg.debug = false;
-        console.log('[INFO]'.cyan + 'Linebot config had successfully loaded!');
-    } else {
-        disable();
-    }
-} catch (err) {
-    disable();
-}
-
 app.use(function(req, res, next) {
     app.locals.moment = moment;
     next();
 });
 
 //linebot callback
-function getSign(data) {
-    var crypto = require('crypto');
-    var body = new Buffer(JSON.stringify(data.body), 'utf8');
-    // secret 為您的 Channel secret
-    var hash = crypto.createHmac('sha256', linebot.cfg.secret).update(body).digest('base64');
-    return hash;
-}
-
-function adduser(lineid) {
-    Line.findOne({
-        id: lineid
-    }).exec(function(err, result) {
-        if (result) {
-            console.log('[ERROR]'.yellow + '[LINEBot]'.green + 'Line id ' + lineid + ' has already existed.');
-            return;
-        }
-        new Line({
-            id: lineid
-        }).save(function(err, r) {
-            if (err) console.log(err);
-        });
-    });
-}
-
-function rmuser(lineid) {
-    Line.findOne({
-        id: lineid
-    }).exec(function(err, result) {
-        if (!result) {
-            console.log('[ERROR]'.yellow + '[LINEBot]'.green + 'Line id ' + lineid + ' doesn\'t exist.');
-            return;
-        }
-        result.remove(function(err, r) {
-            if (err) console.log(err);
-        });
-    });
-}
-
-function multicast(messageData) {
-    request({
-            uri: 'https://api.line.me/v2/bot/message/multicast',
-            headers: {
-                "Content-type": "application/json",
-                "Authorization": "Bearer {" + linebot.cfg.token + "}"
-            },
-            method: 'POST',
-            json: messageData
-        },
-        function(error, response, body) {
-            //Check for error
-            if (error) {
-                return console.log('[ERROR]'.red, error);
-            }
-            //Check for right status code
-            if (response.statusCode !== 200) {
-                return console.log('[ERROR]'.red + 'Invalid Status Code Returned:', response.statusCode, response.statusMessage);
-            }
-            //All is good. Print the body
-            if (linebot.cfg.debug === true) {
-                console.log('[DEBUG]'.cyan + '[LINEBot]'.green + 'req.body'); // Show the HTML for the Modulus homepage.
-                console.log(body);
-            }
-        }
-    );
-}
-
-function sendmes(message) {
-    var data = {
-        to: [],
-        messages: []
-    };
-    Line.find().select('id').exec(function(err, lineid) {
-        var ids = lineid.map(function(res) {
-            return res.id;
-        });
-        data.to = ids;
-        data.messages.push({
-            "type": "text",
-            "text": message
-        });
-        multicast(data);
-    });
-}
 if (linebot.cfg.enable === true) {
     app.post('/callback', function(req, res) {
         var data = req.body;
@@ -225,9 +111,6 @@ if (linebot.cfg.enable === true) {
             res.sendStatus(403); //ChannelSignature錯誤，回傳403
     });
 }
-
-linebot.fun = {};
-linebot.fun.sendmes = sendmes;
 
 app.use('/embed', embed);
 app.use('/', index);
---
+++ linebot.js
@@ -0,0 +1,131 @@
+var request = require('request');
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var Line = mongoose.model('Line');
+var debug = require('debug')('inforann:server');
+
+var ex = {
+    verify: verify,
+    adduser: adduser,
+    rmuser: rmuser,
+    broadcast: broadcast,
+    enable: false,
+    token: '',
+    secret: ''
+};
+
+function disable() {
+    console.log('[WARN]'.yellow + 'unable to read linebot config');
+    console.log('[WARN]'.yellow + 'will disable linebot function');
+    // linebot.cfg = {
+    //     enable: false,
+    //     token: "",
+    //     secret: ""
+    // };
+}
+
+function multicast(messageData) {
+    request({
+            uri: 'https://api.line.me/v2/bot/message/multicast',
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": "Bearer {" + ex.token + "}"
+            },
+            method: 'POST',
+            json: messageData
+        },
+        function(error, response, body) {
+            //Check for error
+            if (error) {
+                return console.log('[ERROR]'.red, error);
+            }
+            //Check for right status code
+            if (response.statusCode !== 200) {
+                return console.log('[ERROR]'.red + 'Invalid Status Code Returned:', response.statusCode, response.statusMessage);
+            }
+            //All is good. Print the body
+            debug('[LINEBot]'.green + 'req.body'); // Show the HTML for the Modulus homepage.
+            debug(body);
+        }
+    );
+}
+
+exports.init = function() {
+    try {
+        var data = fs.readFileSync('linebot.json', {
+            encoding: 'utf-8',
+            flag: 'r'
+        });
+        var cfg = JSON.parse(data);
+        if (cfg.token && cfg.secret) {
+            ex.enable = cfg.enable;
+            ex.token = cfg.token;
+            ex.secret = cfg.secret;
+            console.log('[INFO]'.cyan + 'Linebot config had successfully loaded!');
+        } else {
+            disable();
+        }
+    } catch (err) {
+        disable();
+    }
+    return ex;
+};
+
+function verify(req, onSucceed, onFailed) {
+    var body = new Buffer(JSON.stringify(req.body), 'utf8');
+    var hash = crypto.createHmac('sha256', ex.secret).update(body).digest('base64');
+    if (hash == req.get("x-line-signature")) {
+        onSucceed();
+    } else {
+        if(onFailed)
+            onFailed();
+    }
+}
+
+function adduser(lineid) {
+    Line.findOne({
+        id: lineid
+    }).exec(function(err, result) {
+        if (result) {
+            console.log('[ERROR]'.yellow + '[LINEBot]'.green + 'Line id ' + lineid + ' has already existed.');
+            return;
+        }
+        new Line({
+            id: lineid
+        }).save(function(err, r) {
+            if (err) console.log(err);
+        });
+    });
+}
+
+function rmuser(lineid) {
+    Line.findOne({
+        id: lineid
+    }).exec(function(err, result) {
+        if (!result) {
+            console.log('[ERROR]'.yellow + '[LINEBot]'.green + 'Line id ' + lineid + ' doesn\'t exist.');
+            return;
+        }
+        result.remove(function(err, r) {
+            if (err) console.log(err);
+        });
+    });
+}
+
+function broadcast(message) {
+    var data = {
+        to: [],
+        messages: []
+    };
+    Line.find().select('id').exec(function(err, lineid) {
+        var ids = lineid.map(function(res) {
+            return res.id;
+        });
+        data.to = ids;
+        data.messages.push({
+            "type": "text",
+            "text": message
+        });
+        multicast(data);
+    });
+}
