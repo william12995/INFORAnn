49862368975098e9be6fa4b455eff160979387c0
william12995 <rino881209@gmail.com>
2017-02-17 15:40:32 +0800

    first test 33

--- fbbot.js
+++ fbbot.js
@@ -146,24 +146,3 @@ function adduser(sender) {
     });
 }
 
-function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature"];
-
-  if (!signature) {
-    // For testing, let's log an error. In production, you should throw an 
-    // error.
-    console.error("Couldn't validate the signature.");
-  } else {
-    var elements = signature.split('=');
-    var method = elements[0];
-    var signatureHash = elements[1];
-
-    var expectedHash = crypto.createHmac('sha1', set.appSecret)
-                        .update(buf)
-                        .digest('hex');
-
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
-  }
-}
