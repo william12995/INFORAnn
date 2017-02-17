864491c6de917529c3bffc4d4f33254304d6e716
william12995 <rino881209@gmail.com>
2017-02-17 23:02:31 +0800

    first test 70

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ app.get('/webhook/', function (req, res) {
 
 app.post('/webhook', function (req, res) {
   var data = req.body;
-  console.log( data.messaging) ;
+  console.log( data..entry.messaging) ;
   // Make sure this is a page subscription
   if (data.object == 'page') {
     // Iterate over each entry
