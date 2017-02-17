0519f243df3e2c846b44a38b122f0efa8eb6fcb6
william12995 <rino881209@gmail.com>
2017-02-17 23:15:36 +0800

    first test 74

--- app.js
+++ app.js
@@ -195,9 +195,9 @@ app.post('/webhook/', function (req, res) {
     //
     // You must send back a 200, within 20 seconds, to let us know you've 
     // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
-});
+//     res.sendStatus(200);
+//   }
+// });
 
 
 
