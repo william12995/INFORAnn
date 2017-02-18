3a8fe7676b0307711924fa2475c016b0618bea30
william12995 <rino881209@gmail.com>
2017-02-18 09:29:22 +0800

    first test 88

--- fbbot.js
+++ fbbot.js
@@ -50,7 +50,7 @@ function init(){
 function SingleSendTextMessage(sender, text) {
 	let messageData = { text:text }
 
-	var jizz = ["您的垃圾訊息: ", "別再傳: " ,"傳屁: ","隨便","尤尤","01閔","大猥","妮妮","簡老大","嘻嘻"];
+	var jizz = ["MC", "星星" ,"廣廣","隨便","尤尤","01閔","大猥","妮妮","簡老大","嘻嘻"];
 
 	var count = Math.floor((Math.random() * 10))
 	if (sender == "575623689313399") return;
